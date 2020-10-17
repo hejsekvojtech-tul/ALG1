d03d46b27784dacc00ddd6b702f4ef41c6cef636
hejsekvojtech-tul <vojtech.hejsek@tul.cz>
2020-10-17 15:22:00 +0200

    Tyden 3: Uloha 11
    Signed-off-by: hejsekvojtech-tul <vojtech.hejsek@tul.cz>

---
+++ src/hejsek/apps/KvadratickaRovnice.java
@@ -0,0 +1,42 @@
+package hejsek.apps;
+
+import java.util.Scanner;
+
+public class KvadratickaRovnice {
+    public static void main(String[] args) {
+        double a, b, c = 0;
+
+        final Scanner input = new Scanner(System.in);
+
+        System.out.println("Koeficient a:");
+        a = input.nextDouble();
+        System.out.println("Koeficient b:");
+        b = input.nextDouble();
+        System.out.println("Koeficient c:");
+        c = input.nextDouble();
+
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("Tohle není platná rovnice");
+            } else {
+                double x = -c / b;
+                System.out.format("Řešení rovnice: x = %.5f", x);
+            }
+        } else {
+            double d = Math.pow(b, 2) * - (4 * a * c);
+            double d2 = Math.sqrt(Math.abs(d));
+
+            if (d < 0) {
+                double re = -b / (2 * a);
+                double im = Math.abs(d2 / (2 * a));
+                System.out.format("x1 = %.5f%n", re + im);
+                System.out.format("x2 = %.5f%n", re - im);
+            } else {
+                double x1 = (-b + d2) / (2 * a);
+                double x2 = (-b - d2) / (2 * a);
+                System.out.format("x1 = %.5f%n", x1);
+                System.out.format("x2 = %.5f%n", x2);
+            }
+        }
+    }
+}
