f49250cc8dc3296d13e40505e659d7a38c31457d
hejsekvojtech-tul <vojtech.hejsek@tul.cz>
2020-10-22 10:13:45 +0200

    Uloha 11: Oprava chyby
    Signed-off-by: hejsekvojtech-tul <vojtech.hejsek@tul.cz>

--- src/hejsek/apps/KvadratickaRovnice.java
+++ src/hejsek/apps/KvadratickaRovnice.java
@@ -29,8 +29,8 @@ public class KvadratickaRovnice {
             if (d < 0) {
                 double re = -b / (2 * a);
                 double im = Math.abs(d2 / (2 * a));
-                System.out.format("x1 = %.5f%n", re + im);
-                System.out.format("x2 = %.5f%n", re - im);
+                System.out.format("x1 = %.5f + %.5f%n", re, im);
+                System.out.format("x2 = %.5f - %.5f%n", re - im);
             } else {
                 double x1 = (-b + d2) / (2 * a);
                 double x2 = (-b - d2) / (2 * a);
