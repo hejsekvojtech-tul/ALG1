0ad7e829c01dd47aaba3f7d6d238795964e9c7cc
hejsekvojtech-tul <vojtech.hejsek@tul.cz>
2020-10-24 01:37:12 +0200

    ALG1: KvadratickaRovnice: Uprava pro zadani testu
    Signed-off-by: hejsekvojtech-tul <vojtech.hejsek@tul.cz>

--- src/hejsek/apps/KvadratickaRovnice.java
+++ src/hejsek/apps/KvadratickaRovnice.java
@@ -3,16 +3,14 @@ package hejsek.apps;
 import java.util.Scanner;
 
 public class KvadratickaRovnice {
+    private static final Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
         double a, b, c = 0;
 
-        final Scanner input = new Scanner(System.in);
-
-        System.out.println("Koeficient a:");
+        System.out.println("Zadejte koeficienty a b c%n");
         a = input.nextDouble();
-        System.out.println("Koeficient b:");
         b = input.nextDouble();
-        System.out.println("Koeficient c:");
         c = input.nextDouble();
 
         if (a == 0) {
@@ -23,19 +21,19 @@ public class KvadratickaRovnice {
                 System.out.format("Řešení rovnice: x = %.5f", x);
             }
         } else {
-            double d = Math.pow(b, 2) * - (4 * a * c);
+            double d = Math.pow(b, 2) * (-4 * a * c);
             double d2 = Math.sqrt(Math.abs(d));
 
             if (d < 0) {
                 double re = -b / (2 * a);
                 double im = Math.abs(d2 / (2 * a));
                 System.out.format("x1 = %.5f + %.5f%n", re, im);
-                System.out.format("x2 = %.5f - %.5f%n", re, im);
+                System.out.format("x2 = %.5f - %.5f", re, im);
             } else {
                 double x1 = (-b + d2) / (2 * a);
                 double x2 = (-b - d2) / (2 * a);
                 System.out.format("x1 = %.5f%n", x1);
-                System.out.format("x2 = %.5f%n", x2);
+                System.out.format("x2 = %.5f", x2);
             }
         }
     }
